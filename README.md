f275c222781c09400318528513715e5012d80d13
Debansh-Biswal <99379800+Debansh-Biswal@users.noreply.github.com>
2022-02-10 10:06:21 +0530

    Added new files

---
+++ README.md
@@ -0,0 +1,25 @@
+# Flix Movie Website
+
+    How To Create Responsive Movie Website Using HTML CSS And jQuery
+
+# Preview
+
+!["How To Create Responsive Movie Website Using HTML CSS And jQuery"](https://user-images.githubusercontent.com/67447840/115097268-514c7800-9f53-11eb-9cd0-b4a3126a0978.png "How To Create Responsive Movie Website Using HTML CSS And jQuery")
+
+# Video
+
+    https://youtu.be/4vgkZslEl1w
+
+# Description
+
+    We will make Responsive Movie Website Using HTML CSS And jQuery, Owl Carousel
+
+# Resource
+
+    Boxicons: https://boxicons.com/
+
+    Google font: https://fonts.google.com/
+
+    Owl Carousel: https://owlcarousel2.github.io/OwlCarousel2/docs/started-welcome.html
+
+<iframe src="https://drive.google.com/file/d/134KRU_b9KYqTLaEUgcQqf58EI-kaGy3o/preview" width="640" height="480" allow="autoplay"></iframe>
---
+++ app.js
@@ -0,0 +1,94 @@
+$(document).ready(() => {
+    $('#hamburger-menu').click(() => {
+        $('#hamburger-menu').toggleClass('active')
+        $('#nav-menu').toggleClass('active')
+    })
+
+    // setting owl carousel
+
+    let navText = ["<i class='bx bx-chevron-left'></i>", "<i class='bx bx-chevron-right'></i>"]
+
+    $('#hero-carousel').owlCarousel({
+        items: 1,
+        dots: false,
+        loop: true,
+        nav:true,
+        navText: navText,
+        autoplay: true,
+        autoplayHoverPause: true
+    })
+
+    $('#top-movies-slide').owlCarousel({
+        items: 2,
+        dots: false,
+        loop: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+            500: {
+                items: 3
+            },
+            1280: {
+                items: 4
+            },
+            1600: {
+                items: 6
+            }
+        }
+    })
+
+    $('.movies-slide').owlCarousel({
+        items: 2,
+        dots: false,
+        nav:true,
+        navText: navText,
+        margin: 15,
+        responsive: {
+            500: {
+                items: 2
+            },
+            1280: {
+                items: 4
+            },
+            1600: {
+                items: 6
+            }
+        }
+    })
+})
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
